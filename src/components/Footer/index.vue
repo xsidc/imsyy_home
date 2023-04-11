2e802ffa7b359dfa36093aea06ee3c60db24322f
xsidc <55003092+xsidc@users.noreply.github.com>
2023-04-12 04:25:25 +0800

    Update index.vue

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -3,7 +3,7 @@
     <div class="power" v-show="!store.playerState">
       <span
         >Copyright&nbsp;&copy;&nbsp;{{ fullYear }}
-        <a href="https://imsyy.top">無名</a>
+        <a href="https://wgynav.com">微观云导航</a>
       </span>
       <!-- 以下信息请不要修改哦 -->
       <span class="hidden"
@@ -14,10 +14,6 @@
           {{ config.author }}
         </a></span
       >&nbsp;&amp;
-      <!-- 站点备案 -->
-      <a href="https://beian.miit.gov.cn" target="_blank"
-        >豫ICP备2022018134号-1</a
-      >
     </div>
     <div class="lrc" v-show="store.playerState">
       <music-one theme="filled" size="18" fill="#efefef" />
